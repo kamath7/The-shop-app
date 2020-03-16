28a20f25a24d9bca14d5acfc2de0d43384573f5a
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-03-17 00:52:54 +0530

    Initial setup. Redux store setup

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+
+const ProductsOverviewScreen = (props)=>{
+    return(
+        <FlatList/>
+    );
+};
+
+const styles = StyleSheet.create({});
+export default ProductsOverviewScreen;
