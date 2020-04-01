b75464dfeb2b053dccacbd90893f57166a628c90
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-04-02 04:17:29 +0530

    Added Admin Screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList, Platform} from 'react-native';
+import {View, Text, Button, StyleSheet, FlatList, Platform} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -7,11 +7,19 @@ import {HeaderButtons, Item } from 'react-navigation-header-buttons';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import HeaderButton from '../../components/UI/HeaderButton';
+import Colors from '../../constants/Colors';
+
 
 const ProductsOverviewScreen = (props)=>{
     const products = useSelector(state=> state.products.availableProducts);
     const dispatch = useDispatch();
 
+    const selectItemHandler = (id,title)=>{
+        props.navigation.navigate('ProductDetail',{
+            productId: id,
+            productTitle: title
+        })
+    };
 
     return(
         <FlatList 
@@ -21,16 +29,30 @@ const ProductsOverviewScreen = (props)=>{
                                         image={itemData.item.imageUrl}
                                         title={itemData.item.title}
                                         price={itemData.item.price}
-                                        onViewDetail={()=>{
-                                            props.navigation.navigate('ProductDetail',{
-                                                productId: itemData.item.id,
-                                                productTitle: itemData.item.title
-                                            })
+                                        onSelect={()=>{
+                                            selectItemHandler(itemData.item.id, itemData.item.title);
                                         }}
                                         onAddToCart={()=>{
                                             dispatch(cartActions.addToCart(itemData.item))
                                         }}
-                                    />}
+                                    > 
+                                    <Button
+                                        color={Colors.primary}
+                                        title="View Details"
+                                        onPress={()=>{
+                                            selectItemHandler(itemData.item.id, itemData.item.title);
+                                        }}
+                                    />
+                                    <Button
+                                        color={Colors.primary}
+                                        title="To Cart"
+                                        onPress={()=>{
+                                            dispatch(cartActions.addToCart(itemData.item))
+
+                                        }}
+                                    />
+                                    
+                                    </ProductItem>}
         />
     );
 };
@@ -39,6 +61,14 @@ const styles = StyleSheet.create({});
 ProductsOverviewScreen.navigationOptions = (navData)=>{
     return {
     headerTitle: 'All Products',
+    headerLeft:(
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Menu' iconName={Platform.OS === 'android'? 'md-menu': 'ios-menu'}
+              onPress={()=>{
+                  navData.navigation.toggleDrawer();
+              }}/>
+    </HeaderButtons>
+    ),
     headerRight: (
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
             <Item title='Cart' iconName={Platform.OS === 'android'? 'md-cart': 'ios-cart'}
