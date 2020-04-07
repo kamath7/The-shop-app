b53a14dc43592ab879449f1d5c8ec74b1751e397
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-04-07 18:20:27 +0530

    Admin form validation going on

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {Ionicon, Ionicons} from '@expo/vector-icons';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicon, Ionicons } from '@expo/vector-icons';
 import { Platform } from '@unimodules/core';
 
-const CartItem = (props)=>{
-    return(
+const CartItem = (props) => {
+    return (
         <View style={styles.cartItem}>
             <View style={styles.itemData}>
-                <Text style={styles.quantity}>{props.quantity} </Text> 
+                <Text style={styles.quantity}>{props.quantity} </Text>
                 <Text style={styles.mainText}>{props.title}</Text>
             </View>
             <View style={styles.itemData}>
                 <Text style={styles.mainText}>${props.amount.toFixed(2)}</Text>
-              { props.deletable && <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
-                    <Ionicons name={Platform.OS === 'android'? 'md-trash' : 'ios-trash'} size={23} color="red"/>
+                {props.deletable && <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
+                    <Ionicons name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} size={23} color="red" />
                 </TouchableOpacity>}
             </View>
         </View>
@@ -21,27 +21,27 @@ const CartItem = (props)=>{
 }
 
 const styles = StyleSheet.create({
-    cartItem:{
+    cartItem: {
         padding: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginHorizontal: 20
     },
-    itemData:{
+    itemData: {
         flexDirection: "row",
         alignItems: 'center'
     },
-    quantity:{
+    quantity: {
         fontFamily: 'open-sans',
         color: '#888',
         fontSize: 16
     },
-    mainText:{
+    mainText: {
         fontFamily: 'open-sans-bold',
         fontSize: 16
     },
-    deleteButton:{
+    deleteButton: {
         marginLeft: 20
     }
 });
--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,88 +1,124 @@
-import React , {useState, useEffect, useCallback} from 'react';
-import {View, Text, TextInput, ScrollView, StyleSheet, Platform, Alert} from 'react-native';
-import {HeaderButtons, Item } from 'react-navigation-header-buttons';
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useEffect, useCallback, useReducer } from 'react';
+import { View, Text, TextInput, ScrollView, StyleSheet, Platform, Alert } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import * as productsActions from '../../store/actions/products';
-const EditProductScreen = props=>{
+import Input from '../../components/UI/Input';
 
-    const prodId  = props.navigation.getParam('productId');
-    const editedProduct = useSelector(state=> state.products.userProducts.find(prod => prod.id === prodId));
+
+const FORM_INPUT_UPDATE = 'UPDATE';
+const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        };
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        };
+        let updatedFormIsValid = true;
+        for (const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+        }
+        return {
+            formIsValid: updatedFormIsValid,
+            inputValidities: updatedValidities,
+            inputValues: updatedValues
+        };
+    }
+    return state;
+};
+
+const EditProductScreen = props => {
+
+    const prodId = props.navigation.getParam('productId');
+    const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === prodId));
     const dispatch = useDispatch();
 
-    const [title, setTitle] = useState(editedProduct? editedProduct.title:'');
-    const [price, setPrice] =useState(editedProduct? editedProduct.imageUrl:'');
-    const [imageUrl, setimageUrl] = useState('');
-    const [description, setDescription] = useState(editedProduct? editedProduct.description:'');
-  
-    const submitHandler = useCallback(()=>{
-        if(editedProduct){
-            dispatch(productsActions.updateProduct(prodId, title, description,imageUrl));
-        }else{
-            dispatch(productsActions.createProduct(title, description,imageUrl,+price));
+    const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            title: editedProduct ? editedProduct.title : '',
+            imageUrl: editedProduct ? editedProduct.imageUrl : '',
+            description: editedProduct ? editedProduct.description : '',
+            price: ''
+        },
+        inputValidities: {
+            title: editedProduct ? true : false,
+            imageUrl: editedProduct ? true : false,
+            description: editedProduct ? true : false,
+            price: editedProduct ? true : false
+        },
+        formIsValid: editedProduct ? true : false
+    });
+
+
+    // const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+    // const [titleIsValid, setTitleIsValid] = useState(false);
+    // const [price, setPrice] = useState(editedProduct ? editedProduct.imageUrl : '');
+    // const [imageUrl, setimageUrl] = useState('');
+    // const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
+
+    const submitHandler = useCallback(() => {
+        if (!formState.formIsValid) {
+            Alert.alert('Wrong input', 'Please check the errors in the form.', [{ text: 'Okay' }]);
+            return;
+        }
+        if (editedProduct) {
+            dispatch(productsActions.updateProduct(prodId, formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl));
+        } else {
+            dispatch(productsActions.createProduct(formState.inputValues.title, formState.inputValues.description, formState.inputValues.imageUrl, +formState.inputValues.price));
         }
         props.navigation.goBack();
-    },[dispatch, prodId, title, description, imageUrl, price]);
+    }, [dispatch, prodId, formState]);
 
-    useEffect(()=>{
-        props.navigation.setParams({submit: submitHandler});
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler });
     }, [submitHandler]);
-   
 
-    return(
+    const textChangeHandler = (inputIdentifier, text) => {
+        let isValid = false;
+        if (text.trim().length > 0) {
+            isValid = true;
+        }
+        dispatchFormState({ type: FORM_INPUT_UPDATE, value: text, isValid: isValid, input: inputIdentifier });
+    };
+
+    return (
         <ScrollView>
-        <View style={styles.form}>
-            <View style={styles.formControl}>
-                <Text style={styles.label}>Title</Text>
-                <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
-            </View>
-            <View style={styles.formControl}>
-                <Text style={styles.label}>Image URL</Text>
-                <TextInput style={styles.input} value={imageUrl} onChangeText={text => setimageUrl(text)}/>
-            </View>
-           { editedProduct? null: (<View style={styles.formControl}>
-                <Text style={styles.label}>Price</Text>
-                <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)}/>
-            </View>)}
-            <View style={styles.formControl}>
-                <Text style={styles.label}>Description</Text>
-                <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)}/>
-            </View>
+            <View style={styles.form}>
+                <Input label ="Title" errorText= "Please enter valid title" keyboardType='default' autoCapitalize='sentences' autoCorrect returnKeyType='next' />
+                <Input label ="Image Url" errorText= "Please enter valid Image URL" keyboardType='default' returnKeyType='next' />
+                {editedProduct ? null : (<Input label ="Price" errorText= "Please enter valid price" keyboardType='decimal' returnKeyType='next' />)}
+                <Input label ="Description" errorText= "Please enter valid description" keyboardType='default' autoCapitalize='sentences' autoCorrect multiline numberOfLines={3} />
             </View>
         </ScrollView>
     )
 };
 
-EditProductScreen.navigationOptions = navData=>{
+EditProductScreen.navigationOptions = navData => {
 
     const submitFn = navData.navigation.getParam('submit');
 
-    return{
-        headerTitle: navData.navigation.getParam('productId')? 'Edit Product': 'Add Product',
-        headerRight:  (<HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title='Save' iconName={Platform.OS === 'android'? 'md-checkmark': 'ios-checkmark'}
-              onPress={submitFn}/>
-    </HeaderButtons>)
+    return {
+        headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
+        headerRight: (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Save' iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+                onPress={submitFn} />
+        </HeaderButtons>)
     }
 }
 
 const styles = StyleSheet.create({
-    form:{
+    form: {
         margin: 20
     },
-    formControl:{
-        width: '100%'
-    },
-    label:{
+   
+    label: {
         fontFamily: 'open-sans-bold',
         marginVertical: 8
-    },
-    input:{
-        paddingHorizontal: 2,
-        paddingVertical: 5,
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1
     }
 });
 
